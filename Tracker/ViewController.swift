773083f99afb81b59127105f673a4d0c93cb792c
Олег Аксененко <aksenenko_oyu@drom.ru>
2023-05-28 14:36:25 +1000

    master: Инициализация проекта

---
+++ Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tracker
+//
+//  Created by Олег Аксененко on 28.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
