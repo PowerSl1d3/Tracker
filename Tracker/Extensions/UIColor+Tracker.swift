b141aa7e5b6e54f948b13a69fd596c326a10c397
Олег Аксененко <aksenenko_oyu@drom.ru>
2023-06-18 15:37:40 +1000

    sprint_14: Добавлены цвета и шрифты согласно макетам. Добавлено лого приложения для отображения в магазине приложений

---
+++ Tracker/Extensions/UIColor+Tracker.swift
@@ -0,0 +1,18 @@
+//
+//  UIColor+Tracker.swift
+//  Tracker
+//
+//  Created by Олег Аксененко on 18.06.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    static let ypBlack = UIColor(named: "YP Black")
+    static let ypWhite = UIColor(named: "YP White")
+    static let ypBackground = UIColor(named: "YP Background")
+    static let ypRed = UIColor(named: "YP Red")
+    static let ypBlue = UIColor(named: "YP Blue")
+    static let ypGray = UIColor(named: "YP Gray")
+    static let ypLightGray = UIColor(named: "YP Light Gray")
+}
